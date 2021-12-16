bcd060d71ce2ed5c06da3aecfa20f9ce2ca2dbaf
Kurniawan Adhe Alvianto <79897715+adhe045@users.noreply.github.com>
2021-12-16 13:47:22 +0700

    Add files via upload

--- FPPemrog/FPPemrog/View/LoginWindow.xaml.cs
+++ FPPemrog/FPPemrog/View/LoginWindow.xaml.cs
@@ -22,6 +22,34 @@ namespace FPPemrog.View
         public LoginWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            ResizeMode = ResizeMode.NoResize;
+            txtEmail.Focus();
+        }
+
+        private void txtPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtPassword.Password = "";
+        }
+
+       
+
+        private void txtUsername_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtEmail.SelectionStart = 0;
+            txtEmail.SelectionLength = txtEmail.Text.Length;
+        }
+
+        private void btnLogin_Click_1(object sender, RoutedEventArgs e)
+        {
+            //instance -> create object
+            Controller.LoginController login = new Controller.LoginController(this);
+            //response
+            login.ValidasiLogin();
+            if (login.pesan != "")
+            {
+                MessageBox.Show(login.pesan);
+            }
         }
     }
 }
