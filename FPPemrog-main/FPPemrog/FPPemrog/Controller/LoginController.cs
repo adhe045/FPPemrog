ce75c8728a118056eb34076ed7d42ceb0e469ae9
Krisnaabdi <77990675+Krisnaabdi@users.noreply.github.com>
2021-12-16 13:41:08 +0700

    Add files via upload

---
+++ FPPemrog-main/FPPemrog/FPPemrog/Controller/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPPemrog.Controller
+{
+    class LoginController
+    {
+        //declare object utk class model dan view
+        Model.LoginModel model; // object class model(login)
+        View.LoginWindow view; // object class view (LoginWindow)
+
+        //varibale untuk pesan
+        public string pesan;
+
+        //instance -> constructor
+        public LoginController(View.LoginWindow view)
+        {
+            model = new Model.LoginModel();
+            this.view = view;
+        }
+        //request dan respon -> function biasanya void
+        public void ValidasiLogin()
+        {
+            //data -> diambil dari view (input)
+            model.email = view.txtEmail.Text;
+            model.password = view.txtPassword.Password;
+            //result dari model (true/false)
+            bool hasil = model.checklogin();
+            //response -> view
+            if (hasil)
+            {
+                pesan = "";
+                View.HomeWindow home = new View.HomeWindow();
+                home.Show();
+                view.Close();
+            }
+            else
+            {
+                pesan = "Email/Password salah!";
+                view.txtEmail.Text = "";
+                view.txtPassword.Password = "";
+                view.txtEmail.Focus();
+            }
+        }
+    }
+}
---
+++ FPPemrog-main/FPPemrog/FPPemrog/Model/LoginModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPPemrog.Model
+{
+    class LoginModel
+    {
+        public string email;
+        public string password;
+
+        public bool checklogin()
+        {
+            bool result;
+            if (email == "tono@ymail.com" && password == "12345")
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
